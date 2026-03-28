862fbde4b647d5176ed6f046a336140b75b1974b
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2026-03-28 12:07:07 +0530

    Add ping endpoint that returns 'pong'

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+def ping(request):
+    return HttpResponse("pong")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('reflecta.urls')),
-
+    path('ping/', ping),
+    
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
